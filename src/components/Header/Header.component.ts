cb3b5698826f26f398c8f0a43a2730913e51808e
Isaah <isaaholiveira15@gmail.com>
2023-07-04 02:17:17 -0300

    adicionando formulário de melhorias

--- src/components/Header/Header.component.ts
+++ src/components/Header/Header.component.ts
@@ -1,5 +1,9 @@
+import { ToastrService } from 'ngx-toastr';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { DarkModeService } from 'src/services/DarkMode/DarkMode.service';
+import emailjs from 'emailjs-com';
 
 @Component({
   selector: 'app-Header',
@@ -8,7 +12,11 @@ import { DarkModeService } from 'src/services/DarkMode/DarkMode.service';
 })
 export class HeaderComponent implements OnInit {
   stateDarkMode: boolean = false;
-  constructor(private darkModeService: DarkModeService) { }
+  formGroup: FormGroup;
+  constructor(private darkModeService: DarkModeService, private spinner: NgxSpinnerService, private toastr: ToastrService, private formBuilder: FormBuilder) {
+  this.formGroup = this.formBuilder.group({
+    sugestao: ['', Validators.required]
+  }); }
 
   ngOnInit() {
     this.darkModeService.getIsDarkModeActive().subscribe((isActive: boolean) => {
@@ -19,5 +27,34 @@ export class HeaderComponent implements OnInit {
   onDarkModeActiveChange(isActive: boolean) {
     this.stateDarkMode = isActive;
     this.darkModeService.setIsDarkModeActive(isActive);
+  }
+
+  enviarFormulario(event: Event) {
+    this.spinner.show();
+
+      const serviceID = 'service_mz7dtei';
+      const templateID = 'template_me2news';
+      const userID = 'ugKUR3e3BFOJG0avi';
+
+      const templateParams = {
+        from_name: this.formGroup.value.nome,
+        message: `
+            Olá Isabella! Minha sugestão de melhoria é: ${this.formGroup.get('sugestao')?.value}`
+        };
+
+            setTimeout(() => {
+              this.spinner.hide();
+            }, 2000);
+
+  emailjs.send(serviceID, templateID, templateParams, userID)
+    .then((response) => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+      this.toastr.success('E-mail enviado com sucesso!', 'Sucesso!');
+      this.spinner.hide();
+    })
+    .catch((error) => {
+      this.spinner.hide();
+      this.toastr.error('Erro ao enviar o e-mail:', 'Falha', error);
+    });
   }
 }
